89407988cae4b5a22eefbe43153f3c326deed512
Osamakahen <your.email@example.com>
2025-05-03 07:31:09 +0400

    Test minimal page for 404

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/:path*'
-      }
-    ];
-  },
   typescript: {
     ignoreBuildErrors: false
   }
